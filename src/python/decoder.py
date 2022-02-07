d4eeff6b00bfb5e324970d786cf87ea4f8bb2291
shivrm <shivrams.2006@gmail.com>
2022-02-07 18:37:46 +0530

    Add `bool` and `int` handlers, update `.gitignore`
    Modify `int_type_index` calculation formula to be more performant

--- src/python/decoder.py
+++ src/python/decoder.py
@@ -4,10 +4,41 @@ class DecodeError(Exception):
     pass
 
 def handle_bool(subtype, twine_stream):
-    pass
+    # bool data is stored in the subtype itself and can be determined
+    # without reading from the twine stream
+    if subtype == 0x00:
+        return False
+
+    elif subtype == 0x01:
+        return True
+
+    elif subtype == 0x03:
+        return None
+
+    else:
+        error_msg = f"Unknown bool subtype: {hex(subtype)}"
+        raise DecodeError(error_msg)
 
 def handle_int(subtype, twine_stream):
-    pass
+    
+    # Specifies whether the integer is signed - stored in highest bit
+    is_signed = bool(subtype & 0x08)
+    
+    # Calculate the number of bytes. Last 3 bits of subtype are calculated
+    # using log(byte_count, 2) - 1. We can reverse this to obtain the byte
+    # count from the last 2 digits.
+    byte_count = 2 ** (subtype & 0x07 - 1)
+    
+    # Get data from twine stream into a bytearray
+    data_bytes = bytearray()
+    for _ in range(byte_count):
+        next_byte = next(twine_stream)
+        data_bytes.append(next_byte)
+        
+    # Construct an int from the bytearray
+    decoded_int = int.from_bytes(data_bytes, "big", signed=is_signed)
+    
+    return decoded_int
 
 def handle_float(subtype, twine_stream):
     pass
